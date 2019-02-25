01a73ecbbf5606e59b03dcc8de4404093a6d893b
tiffanykeller <bakertiffany133@yahoo.com>
2019-02-24 20:53:24 -0600

    update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# bamazon
+# Bamazon Application
+
+
+## Overview
+ 
+
+* This is shopping application that uses customer input to order items using database. 
+* Built with javascript, node.js with node packages MySql and inquirer.
+
+
+## Description of App
+
+* The application allows users to see available products for order.
+* User then input the type and quantity of products they want.
+* The database is checked for product availability. If products are available, the order is placed and amount updated in database.
+* Users are notified of order placement and prompted if they would like to place another order.
+. 
+
+## Built With
+
+* Javascript
+* Node.js
+* MySql
+
+
+## Node Packages
+
+* Inquirer
+* MySql
+
+
+## Contributors
+
+* Tiffany Keller
+
+## License
+
+* None
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,169 @@
+//Including the required npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// Creating the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Alaska1!",
+  database: "bamazon"
+});
+
+// Connecting to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log(
+    "You are now connected as id " +
+      connection.threadId +
+      "\n"
+  );
+  // Run the start function after the connection is made to begin the application
+  start();
+});
+
+// Function that prompts the user
+function start() {
+ console.log("PLEASE SELECT YOUR ITEMS");
+  connection.query("SELECT item_id, product_name, price FROM products", function(
+    error,
+    results
+  ) {
+    if (error) throw error;
+    console.log(
+      JSON.stringify(results, null, " ") +
+        "\n\n=========================================================\n\n"
+    );
+
+    // asks user for id and units of products
+    inquirer
+      .prompt([
+        {
+          name: "idChosen",
+          type: "input",
+          message:
+            "Please enter the ID number of the product you wish to purchase from BAMAZON: "
+        },
+        {
+          name: "unitsPurchased",
+          type: "input",
+          message:
+            "How many units of this product would you like to purchase from BAMAZON?"
+        }
+      ])
+      .then(function(answer) {
+        console.log(
+          "\nYOUR ORDER HAS BEEN PLACED!!!\n\nLet me check our stock for you...\n"
+        );
+
+        //variables for chosen products
+        var chosenProductID = answer.idChosen;
+        var chosenProductUnits = answer.unitsPurchased;
+
+        // If there is not enough quantities, return message
+        connection.query(
+          "SELECT * FROM products WHERE ?",
+          [
+            {
+              item_id: chosenProductID
+            }
+          ],
+          function(error, results) {
+            if (error) throw error;
+
+            //This variable stores the current stock quantity of the Chosen product
+            var currentStock = results[0].stock_quantity;
+
+            if (chosenProductUnits > currentStock) {
+              //If BAMAZON doesn't have enough stock
+              console.log(
+                "====ITEM IS OUT OF STOCK, SORRY ====\n"
+              );
+              //Calling the continue
+              continuePrompt();
+            } else {
+              // If BAMAZON does have enough of the product, fulfill the customer's order.
+              console.log(
+                "==== YOUR ORDER HAS BEEN PROCESSED ====\n"
+              );
+
+              //set the total cost and information of the product selected
+              var id = results[0].item_id;
+              var productName = results[0].product_name;
+              var departmentName = results[0].department_name;
+              var price = results[0].price;
+              var originalStock = results[0].stock_quantity;
+              var totalCost = parseFloat(price * chosenProductUnits);
+
+              //Updating the SQL database to reflect the remaining quantity.
+              var newChosenProductStock = currentStock - chosenProductUnits;
+              connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                  {
+                    stock_quantity: newChosenProductStock
+                  },
+                  {
+                    item_id: chosenProductID
+                  }
+                ],
+                function(error, results) {
+                  //UPDATE SUCCESS!
+                  console.log(
+                    "Our product stock quantity of " +
+                      originalStock +
+                      " has been updated to " +
+                      newChosenProductStock +
+                      ".\n"
+                  );
+
+                  //Once the update goes through, show the customer the total cost of their purchase.
+                  console.log(
+                    "=====================================================================================" +
+                      "\nItem number:\t\t\t" +
+                      id +
+                      "\nProduct Name:\t\t\t" +
+                      productName +
+                      "\nProduct Category:\t\t" +
+                      departmentName +
+                      "\nPrice Per Unit:\t\t\t" +
+                      price +
+                      "\nUnits ordered:\t\t\t" +
+                      chosenProductUnits +
+                      "\n-------------------------------------------------" +
+                      "\nYOUR TOTAL COST IS:\t\t$" +
+                      totalCost +
+                      "\n=====================================================================================\n\n"
+                  );
+
+                  //Calling the continue
+                  continuePrompt();
+                }
+              );
+            }
+          }
+        );
+      });
+  });
+} 
+
+//Function to ask the customer if they wish to continue with another purchase, or end
+function continuePrompt() {
+  inquirer
+    .prompt({
+      name: "continue",
+      type: "confirm",
+      message: "Would you like to purchase another product?"
+    })
+    .then(function(answer) {
+      if (answer.continue === true) {
+        start();
+      } else {
+        console.log(
+          "\n==== THANK YOU FOR YOUR ORDER ====\n\n"
+        );
+        connection.end();
+      }
+    });
+}
